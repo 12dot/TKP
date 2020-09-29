706dbbfebfe24c2920b9ea90657c5d2bcc5ac107
12dot <12dot@mail.ru>
2020-09-29 15:10:58 +0400

    Blazor and Todo in Lesson1

---
+++ Lesson1/BlazorApp/Data/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace BlazorApp.Data
+{
+        public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ Lesson1/BlazorApp/Data/TodoListService.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace BlazorApp.Data
+{
+    public class TodoListService{
+        public IList<TodoItem> todos = new List<TodoItem>();
+        public string newTodo;
+        public void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+    }
+
+}
+
